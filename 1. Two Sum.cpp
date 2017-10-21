7025ebf891226e7a2e82d0b2e74fef85231305de
icon97 <p.h.tan97@gmail.com>
2017-10-21 13:14:19 +0700

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> mp;
+        for(int i=0; i<nums.size(); i++) {
+            if(mp[target-nums[i]]) return {mp[target-nums[i]]-1, i};
+            mp[nums[i]] = i+1;
+        }
+        return {0, 0};
+    }
+};
